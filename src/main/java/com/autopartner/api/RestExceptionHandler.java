aa8be2f9a8ffc997770803ff566a973f7a154bbd
Mariana Kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2022-08-10 13:52:32 +0100

    Fix error handler

--- src/main/java/com/autopartner/api/RestExceptionHandler.java
+++ src/main/java/com/autopartner/api/RestExceptionHandler.java
@@ -2,27 +2,48 @@ package com.autopartner.api;
 
 import com.autopartner.api.dto.ErrorResponse;
 import com.autopartner.exception.UserAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import java.util.NoSuchElementException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler {
 
   public static final int ERROR_CODE_FIELD_VALIDATION_FAILED = 402;
 
   @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-  public ErrorResponse handleException(MethodArgumentNotValidException e) {
+  public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException e) {
     String error = e.getBindingResult().getFieldErrors().toString();
-    return new ErrorResponse(BAD_REQUEST.value(), ERROR_CODE_FIELD_VALIDATION_FAILED, error);
+    return error(BAD_REQUEST, ERROR_CODE_FIELD_VALIDATION_FAILED, error);
+  }
+
+  @ExceptionHandler(value = {NoSuchElementException.class})
+  public ResponseEntity<ErrorResponse> handleException(NoSuchElementException e) {
+    return error(NOT_FOUND, NOT_FOUND.value(), e.getMessage());
   }
 
   @ExceptionHandler(value = {UserAlreadyExistsException.class})
-  public ErrorResponse handleException(UserAlreadyExistsException e) {
+  public ResponseEntity<ErrorResponse> handleException(UserAlreadyExistsException e) {
     String error = e.getMessage();
-    return new ErrorResponse(BAD_REQUEST.value(), ERROR_CODE_FIELD_VALIDATION_FAILED, error);
+    return error(BAD_REQUEST, ERROR_CODE_FIELD_VALIDATION_FAILED, error);
+  }
+
+  private ResponseEntity<ErrorResponse> error(HttpStatus status, int code, String message) {
+    final ErrorResponse response = new ErrorResponse(status.value(), code, message);
+    log.info("Error response: {}", response);
+    return ResponseEntity
+        .status(status)
+        .header("Content-Type", "application/json")
+        .body(response);
   }
 
 }
